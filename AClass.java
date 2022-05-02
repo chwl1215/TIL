042b57ed98676c96527f0ab15a5b2cea3e3fe7bd
chwl1215 <chwl1215@naver.com>
2022-05-02 23:37:51 +0900

    중첩클래스

---
+++ AClass.java
@@ -0,0 +1,33 @@
+package interfacequestion;
+
+public class AClass {
+
+		class BClass{
+			//필드
+			int bField;
+			//생성자
+			public BClass() {
+			
+			}
+			//메소드
+			void bMethod() {
+				
+			}
+		}
+		static class CClass { //정적 클래스이기 때문에
+			int cField;
+			static int cField2; //정적 필드 가능
+			//생성자
+			public CClass() {
+				
+			}
+			//메소드
+			void cMethod() {
+				
+			}
+			//정적메소드
+			static void cMethod2() {
+				
+			}
+		}
+}
